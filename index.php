ade0d740e963a200074ee7817e6b91fee9030ce8
designstudio_5 <work@here.com>
2014-06-23 16:53:47 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>Codenista // Web Design + Front-End Development</title>
+    <link href="assets/css/styles.css" rel="stylesheet" />
+</head>
+<body>
+<div id="page" class="container">
+    <header>
+        <h1>Codenista</h1>
+    </header>
+    <div id="content">
+        <section class="first">
+            <h2>GRAB A CUPPA</h2>
+        </section>
+        <!-- sticky navigation bar starts-->
+        <nav>
+            <ul>
+                <li><a href="#index">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+        <!--navigation bar ends-->
+        <section class="second">
+            <p>Now, this is a story all about how
+                My life got flipped-turned upside down
+                And I'd like to take a minute
+                Just sit right there
+                I'll tell you how I became the <del>prince</del> princess of a town called<del> Bel Air</del> Brighton.</p>
+        </section>
+        <section class="third">
+            <a name="#about">
+                <p>About me.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Morbi eu fermentum eros. Curabitur et porttitor ipsum.
+                    Nunc ultrices gravida massa ac tempor.</p>
+            </a>
+        </section>
+        <section class="fourth">
+            <a name="#portfolio">
+                <p>portfolio</p>
+            </a>
+        </section>
+        <section class="fifth">
+            <!-- php contact form starts here-->
+            <a name="#contact">
+                <p>contact</p>
+
+            </a>
+        </section>
+        <section class="sixth">
+            <p>toodles</p>
+        </section>
+
+    </div>
+    <!--footer starts here-->
+    <footer>
+        <nav>
+            <ul>
+                <li> <a href="http://linkedin.com/jessikanygren">Linkedin</a></li>
+                <li> <a href="http://twitter.com/codenista">Twitter</a></li>
+            </ul>
+        </nav>
+    </footer>
+    <!--footer ends here-->
+</div>
+</body>
+</html>
